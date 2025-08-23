c1ff37407157478c0305b4428d3eee934eb5b389
gbnam <gbnam453@gmail.com>
2025-08-24 03:30:12 +0900

    Port Edit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 22031,   // 원하는 포트
+    host: true    // 외부에서도 접근 가능하게
+  }
 })
